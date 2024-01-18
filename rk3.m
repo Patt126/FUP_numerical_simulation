c5984d1b262f84312dcd6a2ae9da67a5e538b0a8
Jack-er-programmatore <156597730+Jack-er-programmatore@users.noreply.github.com>
2024-01-18 17:37:03 +0100

    rk3.m

---
+++ rk3.m
@@ -0,0 +1,94 @@
+clear all
+close all
+format long
+
+N = 32;
+alpha = 0.1;
+n_mode = 5;
+t_max = 10000;
+number_step = 500000;
+dt = t_max/number_step;
+lattice_parameter = 1; %lattice parameter
+T = (0:dt:t_max);
+
+solution = zeros(length(T),2*N); % In a row first N  are speed, last N are position
+mode_0 = zeros(N,1); %Initial displacement in normal modes representation
+v = zeros(length(T),N); % Speed
+x = zeros(length(T),N); % position
+%
+% define normal modes frequencies, coordinates and energy per mode and in
+%time
+omega_k = 2*sin(pi.*(1:n_mode)./(2*(N+1))); %frequencies
+mode_k = zeros(length(T),n_mode); % modes
+energy_k = zeros(length(T),n_mode); %modes' energy
+
+%
+% Define the matrix to pass to from cartesian coordinates to normal modes
+
+A = zeros(N,N);
+for row = 1:N
+    A(row,:) = sqrt(2/(N+1))*sin((pi*row.*(1:N))/(N+1)); 
+end
+mode_0(1) = 1*sqrt(2)/omega_k(1);
+x_0 = A\mode_0;
+initial_condition = zeros(2*N,1);
+initial_condition(N+1:2*N,1) = x_0;
+solution(1,:) = initial_condition';
+
+for i = 1:n_mode
+    mode_k(1,i) = sqrt(2/(N+1))*sum(solution(1,N+1:2*N).*sin(pi*i*(1:N)/(N+1)));
+    energy_k(1,i) = 0.5*(omega_k(i)*mode_k(1,i))^2;
+end
+
+
+%Define  matrix for the system
+linear_part = zeros(N,N);
+next_quadratic = zeros(N,N);
+prev_quadratic = zeros(N,N);
+for i = 1:N
+    linear_part(i,i) = -2;
+    next_quadratic(i,i) = 1;
+    prev_quadratic(i,i) = 1;
+    if i ~= 1
+        linear_part(i,i-1) = 1;
+        prev_quadratic(i,i-1) = -1;
+    end
+    if i ~= N
+        linear_part(i,i+1) = 1;
+        next_quadratic(i,i+1) = -1;
+    end
+end
+identity = diag(ones(N,1));
+
+F = @(t,state) linear_part*state + alpha*((next_quadratic*state).^2-(prev_quadratic*state).^2);
+A = @(t,state) identity*state;
+sol_pos = zeros(length(T),N);
+sol_pos(1,:) = x_0';
+sol_vel = zeros(length(T),N);
+
+for t = 1:length(T)
+   f1 = F(T(t),sol_pos(t,:)');
+   f2 = F(T(t)+dt,sol_pos(t,:)' + dt*f1);
+   f3 = F(T(t)+0.5*dt,sol_pos(t,:)' +0.25*dt*(f1+f2));
+   sol_vel(t+1,:) = sol_vel(t,:) + dt*(f1+f2+4*f3)'/6;
+   g1 = A(T(t),sol_vel(t+1,:)');
+   g2 = A(T(t)+dt,sol_vel(t+1,:)'+dt*g1);
+   g3 = A(T(t)+0.5*dt,sol_vel(t+1,:)'+0.25*dt*(g1+g2));
+   sol_pos(t+1,:) = sol_pos(t,:) + dt*(g1+g2+4*g3)'/6;
+
+end
+solution = [sol_vel,sol_pos];
+for t = 2:length(T)-1
+for k = 1:n_mode
+    mode_k(t,k) = sqrt(2/(N+1))*sum(solution(t,N+1:2*N).*sin(pi*k.*(1:N)/(N+1)));
+    energy_k(t,k) = 0.5*((mode_k(t,k)-mode_k(t-1,k))/dt)^2 + 0.5*(omega_k(k)*mode_k(t,k))^2;
+    end
+end
+
+plot(T,energy_k(:,1),'r-','LineWidth',1);hold on
+plot(T,energy_k(:,2),'k-','LineWidth',1);
+plot(T,energy_k(:,3),'b-','LineWidth',1);
+plot(T,energy_k(:,4),'m-','LineWidth',1);
+plot(T,energy_k(:,5),'g-','LineWidth',1);
+xlabel('t');ylabel('E_{k}');
+legend('mode 1','mode 2','mode 3','mode 4','mode 5')
