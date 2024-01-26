9cf2c9d0e44bf6cbfb8054dcb18d1af73ffdc7f1
Patt126 <145294744+Patt126@users.noreply.github.com>
2024-01-26 22:48:34 +0100

    Update EulerMethod.m

--- Methods/EulerMethod.m
+++ Methods/EulerMethod.m
@@ -6,13 +6,14 @@ v(1,:) = initial_condition(1:N);
 x(1,:) = initial_condition(N+1:2*N);
 
 e = ones(N,1);
-linear_part = spdiags([e -2*e e], -1:1, N, N);
+linear_part = spdiags([e -2*e e], -1:1, N, N); %define differential equation matrix
 next_quadratic = spdiags([e -e], 0:1 , N, N);
 prev_quadratic = spdiags([-e e], -1:0 , N, N);
 
 
 F = @(t,x) MS_cost * linear_part * x + alpha * ((next_quadratic*x).^2-(prev_quadratic*x).^2);
 
+%use partitioned euler scheme
 for t = 2:length(T)
     v(t,:) = v(t-1,:) + dt * F(t-1,x(t-1,:)')';
     x(t,:) = x(t-1,:) + dt * v(t,:);
